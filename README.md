c987899c2e4f30eeef6d0e686117301242fd7570
Alex Marcus <mr@alexmarc.us>
2024-03-28 20:21:52 -0700

    Sets up Storybook, optimizes dev deps

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # React Carousel
 A reusable carousel component with React.
-
-https://dev.to/alexeagleson/how-to-create-and-publish-a-react-component-library-2oe
---
+++ src/Carousel.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import Carousel from './Carousel';
+
+const meta: Meta<typeof Carousel> = {
+  component: Carousel,
+  parameters: {
+    // Optional parameter to center the component in the Canvas. More info: https://storybook.js.org/docs/configure/story-layout
+    layout: 'centered',
+  },
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs']
+};
+
+export default meta;
+type Story = StoryObj<typeof Carousel>;
+
+export const Default: Story = {
+  render: () => <Carousel label="Carousel" />,
+};
